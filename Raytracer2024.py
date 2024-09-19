067e298a83a49d169c85f2ab0d0185410a1a4891
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2024-09-18 23:25:17 -0600

    upd: snowman second iteration
    Refs: RT1

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -15,13 +15,23 @@ rt = RendererRT(screen)
 
 bricks = Material(diffuse=[1.0,0.2,0.2], spec=128, ks=0.25)
 grass = Material(diffuse=[0.2,1.0,0.2], spec=64, ks=0.2)
+button = Material(diffuse=[0.1,0,0], spec=114, ks=0.4)
+snow = Material(diffuse=[0.5,0.9,0.7], spec=364, ks=0.1)
 
 rt.lights.append(DirectionalLight(direction=[-1,-1,-1], intesity=0.8))
 rt.lights.append(DirectionalLight(direction=[0.5,-0.5,-1], intesity=0.8, color=[1,1,1]))
-rt.lights.append(AmbientLight(intesity=0.1))
+rt.lights.append(DirectionalLight(direction=[16,1,2], intesity=0.9, color=[0,1,1]))
+rt.lights.append(AmbientLight(intesity=0.4))
 
-rt.scene.append(Sphere(position=[0, 0, -5], radius=1.5, material=bricks))
-rt.scene.append(Sphere(position=[1, 1, -3], radius=0.5, material=grass))
+# buttons
+rt.scene.append(Sphere(position=[0, 1, -9], radius=0.2, material=button))
+rt.scene.append(Sphere(position=[0, -0.4, -9], radius=0.2, material=button))
+rt.scene.append(Sphere(position=[0, -1.7, -9], radius=0.5, material=button))
+
+# Snowman
+rt.scene.append(Sphere(position=[0, 2.2, -10], radius=0.95, material=snow))
+rt.scene.append(Sphere(position=[0, 0.3, -10], radius=1.2, material=snow))
+rt.scene.append(Sphere(position=[0, -2, -10], radius=1.5, material=snow))
 
 rt.glRender()
 
