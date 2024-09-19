25ab7565dce3fed7f4ceb4febc5f3079e128eb13
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2024-09-18 23:54:54 -0600

    upd: snowman third iteration
    Refs: RT1

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -5,8 +5,8 @@ from figures import *
 from material import Material
 from lights import *
 
-width = 128
-height = 128
+width = 512
+height = 512
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED )
 clock = pygame.time.Clock()
@@ -16,12 +16,26 @@ rt = RendererRT(screen)
 bricks = Material(diffuse=[1.0,0.2,0.2], spec=128, ks=0.25)
 grass = Material(diffuse=[0.2,1.0,0.2], spec=64, ks=0.2)
 button = Material(diffuse=[0.1,0,0], spec=114, ks=0.4)
-snow = Material(diffuse=[0.5,0.9,0.7], spec=364, ks=0.1)
+snow = Material(diffuse=[0.4,0.4,0.5], spec=364, ks=0.66)
+carrot = Material(diffuse=[2,0.9,0.3], spec=91, ks=0.71)
 
 rt.lights.append(DirectionalLight(direction=[-1,-1,-1], intesity=0.8))
 rt.lights.append(DirectionalLight(direction=[0.5,-0.5,-1], intesity=0.8, color=[1,1,1]))
 rt.lights.append(DirectionalLight(direction=[16,1,2], intesity=0.9, color=[0,1,1]))
 rt.lights.append(AmbientLight(intesity=0.4))
+
+# smile
+rt.scene.append(Sphere(position=[-0.4, 1.58, -9], radius=0.05, material=button))
+rt.scene.append(Sphere(position=[0.3, 1.58, -9], radius=0.05, material=button))
+rt.scene.append(Sphere(position=[-0.15, 1.5, -9], radius=0.05, material=button))
+rt.scene.append(Sphere(position=[0.15, 1.5, -9], radius=0.05, material=button))
+
+# eyes
+rt.scene.append(Sphere(position=[0.25, 2.45, -9], radius=0.1, material=button))
+rt.scene.append(Sphere(position=[-0.25, 2.45, -9], radius=0.1, material=button))
+
+# noise
+rt.scene.append(Sphere(position=[0, 1.9, -9], radius=0.25, material=carrot))
 
 # buttons
 rt.scene.append(Sphere(position=[0, 1, -9], radius=0.2, material=button))
