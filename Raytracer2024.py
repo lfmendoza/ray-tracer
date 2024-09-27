af7d805b5165ed4c2c621e8c83f0b0c4584636ce
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2024-09-26 22:26:45 -0600

    feat: implementig figures
    Refs: RT3

--- Raytracer2024.py
+++ Raytracer2024.py
@@ -26,17 +26,18 @@ blueMirror = Material(diffuse=[0.5,0.5,0.1], spec=128, ks=0.2, matType=REFLECTIV
 # marble = Material(texture=Texture("textures/whiteMarble.bmp"), spec=128, ks=0.2, matType=REFLECTIVE)
 glass = Material(spec=128, ks=0.2, ior=1.5, matType=TRANSPARENT)
 water = Material(spec=56, ks=0.4, ior=1.33, matType=TRANSPARENT)
+# woodenBox = Material(texture="textures/woodenBox.bmp")
 
 rt.lights.append(DirectionalLight(direction=[-1,-1,-1], intesity=0.8))
 rt.lights.append(AmbientLight(intesity=0.1))
 
-rt.scene.append(Sphere(position=[2,2,-5], radius=0.9, material=bricks))
-rt.scene.append(Sphere(position=[-2,2,-5], radius=0.9, material=grass))
-rt.scene.append(Sphere(position=[0,-1,-5], radius=0.9, material=mirror))
-rt.scene.append(Sphere(position=[0,1,-5], radius=0.9, material=blueMirror))
-rt.scene.append(Sphere(position=[2,2,-5], radius=0.9, material=glass))
-rt.scene.append(Sphere(position=[-2,-2,-5], radius=0.9, material=water))
-
+# rt.scene.append(Sphere(position=[0,0,-5], radius=1.5, material=glass))
+# rt.scene.append(Plane(position=[0, -5,-5], normal=[0,1,0], material=bricks))
+# rt.scene.append(Disc(position=[0,-1,-5], normal=[0,1,0], radio=1.5, material=mirror))
+rt.scene.append(AABB(position=[1.5,1.5,-5], sizes=[1,1,1], material=bricks))
+rt.scene.append(AABB(position=[-1.5,1.5,-5], sizes=[1,1,1], material=mirror))
+rt.scene.append(AABB(position=[1.5,-1.5,-5], sizes=[1,1,1], material=grass))
+rt.scene.append(AABB(position=[-1.5,-1.5,-5], sizes=[1,1,1], material=glass))
 rt.glRender()
 
 isRunning = True
--- figures.py
+++ figures.py
@@ -1,6 +1,6 @@
 import numpy as np
 from intercept import Intercept
-from math import atan2, acos, pi
+from math import atan2, acos, pi, isclose
 
 class Shape(object):
     def __init__(self, position, material):
@@ -46,3 +46,129 @@ class Sphere(Shape):
         v = acos(normal[1]) / pi
 
         return Intercept(point=P, normal=normal, distance=t0, texCoords=[u, v], rayDirection=dir, obj=self)
+
+class Plane(Shape):
+    def __init__(self, position, normal, material):
+        super().__init__(position=position, material=material)
+        self.normal = normal / np.linalg.norm(normal)
+        self.type = "Plane"
+    
+    def ray_intersect(self, orig, dir):
+        # Distancia = ((planePos - rayOrog) dot normal) / (rayDir dot normal)
+        denom = np.dot(dir, self.normal)
+
+        if isclose(0, denom):
+            return None
+        
+        num = np.dot(np.subtract(self.position, orig), self.normal)
+        t = num / denom
+
+        if t < 0:
+            return None
+        
+        # P = arig - dir * t
+        P = np.add(orig, np.array(dir) * t)
+        return Intercept(point=P,
+                         normal=self.normal,
+                         distance=t,
+                         texCoords=None,
+                         rayDirection=dir,
+                         obj=self)
+class Disc(Plane):
+    def __init__(self, position, normal, radio, material):
+        super().__init__(position=position, normal=normal, material=material)
+        self.radio = radio
+        self.type = "Disc"
+
+    def ray_intersect(self, orig, dir):
+        planeIntercept = super().ray_intersect(orig, dir)
+
+        if planeIntercept is None:
+            return None
+        
+        contact = np.subtract(planeIntercept.point, self.position)
+        contact = np.linalg.norm(contact)
+
+        if contact > self.radio:
+            return None
+        
+        return planeIntercept
+    
+class AABB(Shape):
+    # Axis Aligned Rounding Box
+
+    def __init__(self, position, sizes, material):
+        super().__init__(position, material)
+        self.sizes = sizes
+        self.type = "AABB"
+
+        # Planes
+        self.planes = []
+
+        rightPlane = Plane(position=[position[0] + sizes[0] / 2, position[1], position[2]], normal=[ 1,0,0], material=material)
+        leftPlane = Plane(position=[position[0] - sizes[0] / 2, position[1], position[2]], normal=[ -1,0,0], material=material)
+
+        upPlane = Plane(position=[position[0], position[1] + sizes[1] / 2, position[2]], normal=[0,1,0], material=material)
+        downPlane = Plane(position=[position[0], position[1] - sizes[0] / 2, position[2]], normal=[0,-1,0], material=material)
+
+        frontPlane = Plane([position[0], position[1], position[2] + sizes[2] / 2], [0,0,1], material=material)
+        backPlane = Plane([position[0], position[1], position[2] - sizes[2] / 2], [0,0,-1], material=material)
+
+        self.planes.append(rightPlane)
+        self.planes.append(leftPlane)
+        self.planes.append(upPlane)
+        self.planes.append(downPlane)
+        self.planes.append(frontPlane)
+        self.planes.append(backPlane)
+
+        # Bounds
+        self.boundsMin = [0,0,0]
+        self.boundsMax = [0,0,0]
+
+        epsilon = 0.001
+
+        for i in range(3):
+            self.boundsMin[i] = position[i] - (epsilon + sizes[i]/2)
+            self.boundsMax[i] = position[i] + (epsilon + sizes[i]/2)
+
+    def ray_intersect(self, orig, dir):
+        intercept = None
+        t = float("inf")
+        for plane in self.planes:
+            planeIntercept = plane.ray_intersect(orig, dir)
+
+            if planeIntercept is not None:
+                planePoint = planeIntercept.point
+
+                if self.boundsMin[0] <= planePoint[0] <= self.boundsMax[0]:
+                    if self.boundsMin[1] <= planePoint[1] <= self.boundsMax[1]:
+                        if self.boundsMin[2] <= planePoint[2] <= self.boundsMax[2]:
+                            if planeIntercept.distance < t:
+                                t = planeIntercept.distance
+                                intercept = planeIntercept
+        if intercept == None:
+            return None
+        
+        u, v = 0, 0
+        if abs(intercept.normal[0]) > 0:
+            # Mapear las uvs para el eje X, usando las coordenadas de Y y Z
+            u = (intercept.point[1] - self.boundsMin[1]) / self.sizes[1]
+            v = (intercept.point[2] - self.boundsMin[2]) / self.sizes[2]
+        elif abs(intercept.normal[1]) > 0:
+            # Mapear las uvs para el eje Y, usando las coordenadas de X y Z
+            u = (intercept.point[0] - self.boundsMin[0]) / self.sizes[0]
+            v = (intercept.point[2] - self.boundsMin[2]) / self.sizes[2]
+        elif abs(intercept.normal[2]) > 0:
+            # Mapear las uvs para el eje Z, usando las coordenadas de X y Y
+            u = (intercept.point[0] - self.boundsMin[0]) / self.sizes[0]
+            v = (intercept.point[1] - self.boundsMin[1]) / self.sizes[1]
+        
+        u = min(0.999, max(0, u))
+        v = min(0.999, max(0, v))
+        
+        return Intercept(point = intercept.point,
+                         normal=intercept.normal,
+                         distance=t,
+                         texCoords=[u, v],
+                         rayDirection=dir,
+                         obj=self)
